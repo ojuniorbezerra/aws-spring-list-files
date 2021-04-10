3e46ba3dcfc53058fa58b04a0b6b9ef4fb2a1990
Junior Bezerra <junior.bezerra@hp.com>
2021-04-10 13:43:08 -0300

    build project

---
+++ src/main/java/com/example/cloud/listfiles/ListfilesApplication.java
@@ -0,0 +1,13 @@
+package com.example.cloud.listfiles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListfilesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListfilesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/cloud/listfiles/ListfilesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cloud.listfiles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListfilesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
